337f708011323f6bd1af858558ac801f87c7e4d9
Abdourahamane BOINAIDI <abdourahamane.boinaidi@infomaniak.com>
2020-08-26 09:40:16 +0200

    webview login activity lib + demo

--- Example/src/main/java/com/infomaniak/login/example/MainActivity.kt
+++ Example/src/main/java/com/infomaniak/login/example/MainActivity.kt
@@ -6,13 +6,19 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.infomaniak.lib.login.InfomaniakLogin
+import com.infomaniak.lib.login.WebViewLoginActivity
 import com.infomaniak.login.example.BuildConfig.APPLICATION_ID
 import com.infomaniak.login.example.BuildConfig.CLIENT_ID
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.loginButton
+import kotlinx.android.synthetic.main.activity_main.webViewLoginButton
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var infomaniakLogin: InfomaniakLogin
+
+    companion object{
+        private const val WEB_VIEW_LOGIN_REQ = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +46,24 @@ class MainActivity : AppCompatActivity() {
             infomaniakLogin.start()
         }
 
+        webViewLoginButton.setOnClickListener {
+            val intent = Intent(this, WebViewLoginActivity::class.java).apply {
+                putExtra(WebViewLoginActivity.CLIENT_ID_TAG, BuildConfig.CLIENT_ID)
+                putExtra(WebViewLoginActivity.APPLICATION_ID_TAG, BuildConfig.APPLICATION_ID)
+            }
+            startActivityForResult(intent, WEB_VIEW_LOGIN_REQ)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == WEB_VIEW_LOGIN_REQ && resultCode == RESULT_OK) {
+            val code = data?.extras?.getString(WebViewLoginActivity.CODE_TAG)
+            val error = data?.extras?.getString(WebViewLoginActivity.ERROR_TAG)
+            Log.e("WebView code", code ?: "")
+            Log.e("WebView error", error ?: "")
+        }
     }
 
     public override fun onDestroy() {
