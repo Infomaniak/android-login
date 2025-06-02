9d15f9d33b9ba33318b0ef2f68ddf341bc1d9671
Louis CAD <louis.cognault@infomaniak.com>
2025-06-02 17:12:13 +0200

    chore: Migrate root project's build file to Gradle Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,30 @@
+/*
+ * Infomaniak Login - Android
+ * Copyright (C) 2025 Infomaniak Network SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 buildscript {
-    ext.kotlin_version = '2.1.20'
-    ext.java_version = JavaVersion.VERSION_17
+    val kotlin_version by extra("2.1.20")
+    val java_version by extra(JavaVersion.VERSION_17)
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.8.2'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:8.8.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     }
 }
 
